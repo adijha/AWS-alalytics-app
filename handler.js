39c195cd252ea20cd7a5463bca351d6c8f85ef98
Aditya Kumar Jha <adityajha126@gmail.com>
2019-08-30 02:25:44 +0530

    cube

--- handler.js
+++ handler.js
@@ -1,18 +1,43 @@
-'use strict';
+const AWS = require('aws-sdk');
+const { promisify } = require('util');
 
-module.exports.hello = async event => {
+const kinesis = new AWS.Kinesis();
+
+const putRecord = promisify(kinesis.putRecord.bind(kinesis));
+
+const response = (body, status) => {
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+    statusCode: status || 200,
+    body: body && JSON.stringify(body),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+      'Content-Type': 'application/json'
+    }
+  }
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.collect = async (event, context) => {
+  const body = JSON.parse(event.body);
+  if (!body.anonymousId || !body.url || !body.eventType) {
+    return response({
+      error: 'anonymousId, url and eventType required'
+    }, 400);
+  }
+
+  await putRecord({
+    Data: JSON.stringify({
+      anonymous_id: body.anonymousId,
+      url: body.url,
+      event_type: body.eventType,
+      referrer: body.referrer,
+      timestamp: (new Date()).toISOString(),
+      source_ip: event.requestContext.identity.sourceIp,
+      user_agent: event.requestContext.identity.userAgent
+    }) + '\n',
+    PartitionKey: body.anonymousId,
+    StreamName: 'event-collection'
+  });
+
+  return response();
 };
